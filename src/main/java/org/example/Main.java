fddcf1e0e65a35a87a19f2123f65f0738d1b12fa
Oleksandr <mir.aleksandr.m@gmail.com>
2022-12-17 16:27:07 +0200

    Mod 8

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+
+        System.out.println(shape.getShapeName(NameShape.Circle));
+    }
+}
---
+++ src/main/java/org/example/NameShapes.java
@@ -0,0 +1,45 @@
+package org.example;
+
+class Circle extends Shape {
+    protected static String getName(){
+        return "Circle";
+    }
+
+}
+class Quad extends Shape {
+    public static String getName(){
+        return "Quad";
+    }
+
+}
+class Qube extends Shape {
+    public static String getName(){
+        return "Qube";
+    }
+
+}
+class Pyramid extends Shape {
+    public static String getName(){
+        return "Pyramid";
+    }
+
+}
+class Cone extends Shape {
+    public static String getName(){
+        return "Cone";
+    }
+
+}
+class Rectangle extends Shape {
+    public static String getName(){
+        return "Rectangle";
+    }
+
+}
+class Trapezium extends Shape {
+    public static String getName(){
+        return "Trapezium";
+    }
+
+}
+
